fd7f79aa214e6488a05a6ded2aa07f005448a8c5
MGaloto <maxigaloto@hotmail.com>
2022-01-05 19:58:12 -0300

    avanzando

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 
 
-library(shiny)
-library(shinyWidgets)
+
 
 # ui ------------------------------------------------------------------
 
@@ -9,33 +8,36 @@ ui <- fluidPage(
     setBackgroundColor("#191970"),
     br(),
     img(src = 'ia.png', width = '100%', height = 150),
-    h1('Data Science Panel Control', style = "color:#27a38a", align = 'left'),
+    h1('Financial Panel', style = "color:#27a38a", align = 'left'),
     sidebarLayout(
         sidebarPanel(style = "color:#27a38a",
-                     tags$style(type = 'text/css','.well {
-                       background-color: #00244a;}'),
-            tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: purple}")),
+                     tags$style(type = 'text/css','.well {background-color: #00244a;}'),
+
             
 # slider ------------------------------------------------------------------
+            
 
-            sliderInput(inputId = "slider",
-                        label = "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
+            sliderTextInput(inputId = "trajectory", 
+                            label = "Date Range:", 
+                            choices = choices,
+                            selected = c(default, max(choices)))
 
-# select ------------------------------------------------------------------
 
-            selectInput(inputId = "select",
-                        label = p('Seleccione la variable: ',  style="color:#27a38a"),
-                        choices = c('Grafico 1','Grafico 2','Grafico 3'))
+# mainpanel ---------------------------------------------------------------
+
+
         ),
         mainPanel(
             tabsetPanel(type = 'pills',
                         
+
+# primer tab ---------------------------------------------------------------
+
+                        
                         tabPanel('Primer Tab',
                                  fluidRow(
-                                     p('Primero'))),
+                                   br(),
+                                   highchartOutput("valuetech",height = "500", width = '1000'))),
                         
                         tabPanel('Segundo Tab',
                                  fluidRow(
@@ -67,10 +69,50 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
+  
+  output$valuetech <- renderHighchart({
+    
+
+# primer tab plot ---------------------------------------------------------------
+    
+    
+    MSFTdis = round((MSFT$Close[MSFT$Fecha == input$trajectory[2]] - MSFT$Close[MSFT$Fecha == input$trajectory[1]])  / MSFT$Close[MSFT$Fecha == input$trajectory[1]],4)*100
+    GOOGLdis = round((GOOGL$Close[GOOGL$Fecha == input$trajectory[2]] - GOOGL$Close[GOOGL$Fecha == input$trajectory[1]])  / GOOGL$Close[GOOGL$Fecha == input$trajectory[1]],4)*100
+    AAPLdis = round((AAPL$Close[AAPL$Fecha == input$trajectory[2]] - AAPL$Close[AAPL$Fecha == input$trajectory[1]])  / AAPL$Close[AAPL$Fecha == input$trajectory[1]],4)*100
+    AMZNdis = round((AMZN$Close[AMZN$Fecha == input$trajectory[2]] - AMZN$Close[AMZN$Fecha == input$trajectory[1]])  / AMZN$Close[AMZN$Fecha == input$trajectory[1]],4)*100
+    
+    
+    Acciones = c("GOOGL","MSFT","AAPL","AMZN")
+    
+    Variacion = c(GOOGLdis,MSFTdis,AAPLdis,AMZNdis)
+    
+    df = data.frame(Acciones, Variacion) %>% arrange(-Variacion)
+    
+    
+    bar_plot = df %>% 
+      arrange(desc(Variacion)) %>% 
+      hchart('column', hcaes(x = Acciones, y = Variacion, color = custom)) %>% 
+      hc_add_theme(hc_theme_flatdark()) %>% 
+      hc_tooltip(pointFormat = '<b>Variacion %: </b> {point.y} <br> ' ) %>% 
+      hc_title(text = 'Acciones Tecnologicas',
+               style = list(fontSize = '25px', fontWeight = 'bold')) %>% 
+      hc_subtitle(text = paste('Variacion % desde:  ',input$trajectory[1], ' hasta:  ',input$trajectory[2]),
+                  style = list(fontSize = '16px', color = "#ffffff")) %>% 
+      hc_credits(enabled = TRUE, text = '@MGaloto',
+                 style = list(fontSize = '16px', color = "#ffffff")) %>% 
+      hc_yAxis(labels = list(style = list(color = "#ffffff")),
+               tickColor = "#ffffff") %>% 
+      hc_xAxis(labels = list(style = list(color = "#ffffff")),
+               tickColor = "#ffffff")
+    
+    bar_plot
+    
+# primer tab cierre ---------------------------------------------------------------
+
+  })
 
 
 }
-
 
 
 
---
+++ global.R
@@ -0,0 +1,147 @@
+
+# Librerias
+
+library(corrplot) 
+library(quantmod)
+library(highcharter)
+library(tidyr)
+library(ggplot2)
+library(readr)  
+library(dplyr) 
+library(plotly)
+library(crayon) 
+library(modeest)
+library(readxl)
+library(plotly)
+library(ggthemes)
+library(reshape)
+library(viridisLite)
+library(viridis)
+library(shinyWidgets)
+library(lubridate)
+
+# --------------------------------------------------------------------------------------- #
+
+# Importar acciones
+
+AAPL=as.data.frame(getSymbols("AAPL" , src = 'yahoo', auto.assign = F, from = "2004-08-19", to = Sys.Date(), periodicity = "daily"))
+AMZN=as.data.frame(getSymbols("AMZN" , src = 'yahoo', auto.assign = F, from = "2004-08-19", to = Sys.Date(), periodicity = "daily"))
+MSFT=as.data.frame(getSymbols("MSFT" , src = 'yahoo', auto.assign = F, from = "2004-08-19", to = Sys.Date(), periodicity = "daily"))
+GOOGL=as.data.frame(getSymbols("GOOGL" , src = 'yahoo', auto.assign = F, from = "2004-08-19", to = Sys.Date(), periodicity = "daily"))
+
+
+# --------------------------------------------------------------------------------------- #
+
+# Se agregan las Fechas como variables
+
+AAPL$Fecha <- row.names(AAPL)
+AMZN$Fecha <- row.names(AMZN)
+MSFT$Fecha <- row.names(MSFT)
+GOOGL$Fecha <- row.names(GOOGL)
+
+
+# --------------------------------------------------------------------------------------- #
+
+# Se mofidican las columnas
+
+colnames(AAPL) = c("Open", "High", "Low", "Close", "Volumen", "Ajustado", "Fecha")
+colnames(AMZN) = c("Open", "High", "Low", "Close", "Volumen", "Ajustado", "Fecha")
+colnames(MSFT) = c("Open", "High", "Low", "Close", "Volumen", "Ajustado", "Fecha")
+colnames(GOOGL) = c("Open", "High", "Low", "Close", "Volumen", "Ajustado", "Fecha")
+
+# --------------------------------------------------------------------------------------- #
+
+# Se numeran las filas
+
+rownames(AAPL) = 1:nrow(AAPL)
+rownames(AMZN) = 1:nrow(AMZN)
+rownames(MSFT) = 1:nrow(MSFT)
+rownames(GOOGL) = 1:nrow(GOOGL)
+
+
+# --------------------------------------------------------------------------------------- #
+
+# Fechas formatos
+
+
+GOOGL$Fecha = as.Date(GOOGL$Fecha)
+AAPL$Fecha = as.Date(AAPL$Fecha)
+AMZN$Fecha = as.Date(AMZN$Fecha)
+MSFT$Fecha = as.Date(MSFT$Fecha)
+
+
+# --------------------------------------------------------------------------------------- #
+
+# Variables de prueba
+
+
+
+MSFTdis = round((MSFT$Close[MSFT$Fecha == '2022-01-04'] - MSFT$Close[MSFT$Fecha == '2020-09-24'])  / MSFT$Close[MSFT$Fecha == '2020-09-24'],4)*100
+GOOGLdis = round((GOOGL$Close[GOOGL$Fecha == '2022-01-04'] - GOOGL$Close[GOOGL$Fecha == '2020-09-24'])  / GOOGL$Close[GOOGL$Fecha == '2020-09-24'],4)*100
+AAPLdis = round((AAPL$Close[AAPL$Fecha == '2022-01-04'] - AAPL$Close[AAPL$Fecha == '2020-09-24'])  / AAPL$Close[AAPL$Fecha == '2020-09-24'],4)*100
+AMZNdis = round((AMZN$Close[AMZN$Fecha == '2022-01-04'] - AMZN$Close[AMZN$Fecha == '2020-09-24'])  / AMZN$Close[AMZN$Fecha == '2020-09-24'],4)*100
+
+
+
+
+Acciones = c(
+  "GOOGL",
+  "MSFT",
+  "AAPL",
+  "AMZN")
+
+
+Variacion = c(
+  GOOGLdis,
+  MSFTdis,
+  AAPLdis,
+  AMZNdis)
+
+df = data.frame(Acciones, Variacion) %>% arrange(-Variacion)
+
+
+# --------------------------------------------------------------------------------------- #
+
+# Grafico de barras de prueba 
+
+custom = viridis::mako(n = 4)
+
+bar_plot = df %>% 
+  arrange(desc(Variacion)) %>% 
+  hchart('column', hcaes(x = Acciones, y = Variacion, color = custom)) %>% 
+  hc_add_theme(hc_theme_flatdark()) %>% 
+  hc_tooltip(pointFormat = '<b>Variacion %: </b> {point.y} <br> ' ) %>% 
+  hc_title(text = 'Acciones Tecnologicas',
+           style = list(fontSize = '25px', fontWeight = 'bold')) %>% 
+  hc_subtitle(text = 'Dinamica',
+              style = list(fontSize = '16px', color = "#ffffff")) %>% 
+  hc_credits(enabled = TRUE, text = '@MGaloto',
+             style = list(fontSize = '16px', color = "#ffffff")) %>% 
+  hc_yAxis(labels = list(style = list(color = "#ffffff")),
+           tickColor = "#ffffff") %>% 
+  hc_xAxis(labels = list(style = list(color = "#ffffff")),
+           tickColor = "#ffffff")
+  
+
+
+# --------------------------------------------------------------------------------------- #
+
+# Editando las fechas 
+
+
+
+choices <- seq.Date(as.Date("2004-08-19"), today() - 1, by = 1)
+
+choices <- choices[!wday(choices) %in% c(1, 7)] 
+
+default <- seq.Date(today() - 182, today() - 180, by = 1) 
+
+default <- default[!wday(default) %in% c(1, 7)]
+
+default <- max(default) 
+
+
+
+
+
+
